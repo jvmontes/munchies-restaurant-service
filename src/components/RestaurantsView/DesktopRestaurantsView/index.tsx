2fdad18a1fcb6313073bfa62c230971f5f3bd7f1
Jorge V <>
2025-11-21 10:00:12 -0600

    Feature: Add Munchies logo to top of app; remove unused images.

--- src/components/RestaurantsView/DesktopRestaurantsView/index.tsx
+++ src/components/RestaurantsView/DesktopRestaurantsView/index.tsx
@@ -1,11 +1,19 @@
 import { FilterType } from "@/src/app/types/restaurant";
+import Image from "next/image";
 import FilterCardCollection from "../FilterCardCollection";
 import Restaurants from "../Restaurants";
 
 export default function DesktopRestaurantsView({ filters }: { filters: FilterType[] }) {
 
     return <div>
-        <h1>[logo] Munchies</h1>
+        <Image
+            src="/logos/Vector.svg"
+            alt="Munchies"
+            width={274}
+            height={40}
+            priority
+            className="pt-16"
+        />
         //side bar
         <p>Side Bar</p>
         <FilterCardCollection filters={filters} />
--- src/components/RestaurantsView/MobileRestaurantsView/index.tsx
+++ src/components/RestaurantsView/MobileRestaurantsView/index.tsx
@@ -1,11 +1,18 @@
 import { FilterType } from "@/src/app/types/restaurant";
+import Image from "next/image";
 import FilterCardCollection from "../FilterCardCollection";
 import Restaurants from "../Restaurants";
 
 export default function MobileRestaurantsView({ filters }: { filters: FilterType[] }) {
     return <div className="w-full overflow-x-hidden">
-        <div className="px-4">
-            <h1>[logo] Munchies</h1>
+        <div className="px-4 pt-14">
+            <Image
+                src="/logos/Vector.svg"
+                alt="Munchies"
+                width={168}
+                height={24}
+                priority
+            />
             <h2>Delivery Time</h2>
             <p>Delivery Time Cards</p>
         </div>
